d920a2f28d5e5e27e1e289055bc47ce0df50c37e
Elliott Coleman <elliott.coleman@yld.io>
2025-02-17 16:14:15 +0000

    feat(LIVE-16730) use ScopeProvider on all pages, return Dapp account for * currencies

--- apps/ledger-live-mobile/src/screens/Swap/LiveApp/WebView.tsx
+++ apps/ledger-live-mobile/src/screens/Swap/LiveApp/WebView.tsx
@@ -37,8 +37,8 @@ export function WebView({ manifest, setWebviewState }: Props) {
 
   // ScopeProvider required to prevent conflicts between Swap's Webview instance and deeplink instances
   return (
-    <Flex flex={1}>
-      <ScopeProvider atoms={[currentAccountAtom]}>
+    <ScopeProvider atoms={[currentAccountAtom]}>
+      <Flex flex={1}>
         <Web3AppWebview
           manifest={manifest}
           customHandlers={customHandlers}
@@ -56,7 +56,7 @@ export function WebView({ manifest, setWebviewState }: Props) {
             OS: Platform.OS,
           }}
         />
-      </ScopeProvider>
-    </Flex>
+      </Flex>
+    </ScopeProvider>
   );
 }
