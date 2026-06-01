0fc6fdd47d68ffafbe81c159e1fac07f698badb2
Sebastien David <sebastien.david@ledger.fr>
2026-06-01 11:38:54 +0200

    feat: add flagRow
    feat: wire flagEnableIndicator into flagRow

--- devtools/feature-flags/src/components/index.ts
+++ devtools/feature-flags/src/components/index.ts
@@ -1,3 +1,4 @@
 export { Pill } from "./pill/Pill";
 export type { PillVariant, PillProps } from "./pill/Pill";
+export { FlagRow } from "./flagRow/FlagRow";
 export { FlagEnableIndicator } from "./flagEnableIndicator/FlagEnableIndicator";
