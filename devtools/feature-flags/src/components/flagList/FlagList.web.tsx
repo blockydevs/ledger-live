d4cffaac3b25dc007c6177630044ee1253445865
Sebastien David <sebastien.david@ledger.fr>
2026-06-02 14:46:05 +0200

    feat(devtools/feature-flags): open sidebar on flag selection

--- devtools/feature-flags/src/components/flagList/FlagList.web.tsx
+++ devtools/feature-flags/src/components/flagList/FlagList.web.tsx
@@ -1,15 +1,20 @@
+import { Fragment } from "react";
 import type { FeatureId } from "@shared/feature-flags";
 import type { FeatureFlagsToolProps } from "../../types";
 import { FlagRow } from "../flagRow/FlagRow.web";
 import { ALL_FLAG_IDS } from "../../constants";
 import { FlagListHeader } from "../flagListHeader/FlagListHeader";
 import { useFeatureFlagsState } from "../../hooks/useFeatureFlagsState";
+import { useFlagSelection } from "../../hooks/useFlagSelection";
 import { Divider } from "@ledgerhq/lumen-ui-react";
+import { Sidebar } from "../sidebar/Sidebar";
 
 export const FlagList = (props: FeatureFlagsToolProps) => {
   const { overrides, setOverride } = props;
   const { getFlagDisplayState } = useFeatureFlagsState(props);
+  const { selectedFlagId, selectFlag, clearSelection } = useFlagSelection();
   const featureIds: FeatureId[] = ALL_FLAG_IDS;
+
   return (
     <>
       <FlagListHeader
@@ -18,16 +23,23 @@ export const FlagList = (props: FeatureFlagsToolProps) => {
         numberOfFilteredFlags={featureIds.length} // no filter implemented yet
       />
       <Divider className="bg-canvas-muted" />
+      {selectedFlagId && (
+        <Sidebar
+          setOverride={setOverride}
+          display={getFlagDisplayState(selectedFlagId)}
+          onClose={clearSelection}
+        />
+      )}
       <div>
         {featureIds.map(featureId => (
-          <div key={featureId}>
+          <Fragment key={featureId}>
             <FlagRow
-              key={featureId}
               display={getFlagDisplayState(featureId)}
               setOverride={setOverride}
+              onSelect={() => selectFlag(featureId)}
             />
-            <Divider key={`${featureId}-divider`} className="bg-canvas-muted" />
-          </div>
+            <Divider className="bg-canvas-muted" />
+          </Fragment>
         ))}
       </div>
     </>
--- devtools/feature-flags/src/hooks/index.ts
+++ devtools/feature-flags/src/hooks/index.ts
@@ -2,3 +2,5 @@ export { useFeatureFlagsState } from "./useFeatureFlagsState";
 export type { FeatureFlagsToolState } from "./useFeatureFlagsState";
 export { useFeatureFlagsFilters } from "./useFeatureFlagsFilters";
 export type { FeatureFlagsFiltersState, FeatureFlagsFiltersInput } from "./useFeatureFlagsFilters";
+export { useFlagSelection } from "./useFlagSelection";
+export type { FlagSelectionState } from "./useFlagSelection";
