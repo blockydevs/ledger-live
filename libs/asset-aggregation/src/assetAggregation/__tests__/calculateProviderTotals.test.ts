93b36e8128bd81036ff716081bc8ded66196b9cd
ysitbon <yoann.sitbon@ledger.fr>
2026-06-15 11:51:42 +0200

    fix: correct bscUsdcToken parentCurrencyId from ethereum to bsc in asset-aggregation test

--- libs/asset-aggregation/src/assetAggregation/__tests__/calculateProviderTotals.test.ts
+++ libs/asset-aggregation/src/assetAggregation/__tests__/calculateProviderTotals.test.ts
@@ -27,7 +27,7 @@ describe("calculateProviderTotals", () => {
     type: "TokenCurrency",
     id: "bsc/bep20/usdc",
     contractAddress: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
-    parentCurrencyId: ethCurrency.id,
+    parentCurrencyId: "bsc",
     tokenType: "bep20",
     name: "USD Coin (BSC)",
     ticker: "USDC",
