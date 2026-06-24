919781aec2ddc0efc9af6298efc6b61e7583fd2f
Victor Alber <victor.alber@ledger.fr>
2026-06-24 16:20:46 +0200

    fix: moving tags to spec files to improve filtering on CI

--- e2e/mobile/specs/delegate/voteCELO.spec.ts
+++ e2e/mobile/specs/delegate/voteCELO.spec.ts
@@ -2,4 +2,8 @@ import { Account } from "@ledgerhq/live-common/e2e/enum/Account";
 import { runVoteCelo } from "./delegate";
 
 const delegation = new Delegate(Account.CELO_1, "0.001", "cLabs");
-runVoteCelo(delegation, ["B2CQA-201"]);
+runVoteCelo(
+  delegation,
+  ["B2CQA-201"],
+  ["@NanoSP", "@NanoX", "@Stax", "@Flex", "@NanoGen5", `@celo`, `@family-celo`],
+);
